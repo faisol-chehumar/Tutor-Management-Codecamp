9a4380cb41e390ec01a021cb17c91d430099f8c6
faisol-chehumar <30441151+faisol-chehumar@users.noreply.github.com>
2018-09-23 14:40:58 +0700

    staff api

--- tutor-api/src/repository/staff.js
+++ tutor-api/src/repository/staff.js
@@ -1,27 +1,39 @@
 const pool = require('../db')
 
 const get = async () => {
-  const result = await pool.query(`
-    SELECT * 
-    FROM staff
-    LEFT JOIN 
-      map_markers 
-    ON
-      staff.map_marker_id = map_markers.map_marker_id;
-  `)
+  try {
+    const result = await pool.query(`
+      SELECT * 
+      FROM staff
+      LEFT JOIN 
+        map_markers 
+      ON
+        staff.map_marker_id = map_markers.map_marker_id;
+    `)
+    
+    return result[0]
 
-	return result[0]
+  } catch(error) {
+    console.error(error)
+    return error
+  }
 }
 
 const insert = async (firstname, lastname, email, tel, map_marker_id) => {
-	const result = await pool.query(`
-		insert into staff
-      (firstname, lastname, email, tel, map_marker_id)
-		values
-			(?, ?, ?, ?, ?)
-	`, [ firstname, lastname, email, tel, map_marker_id ])
+  try {
+    const result = await pool.query(`
+      INSERT INTO staff
+        (firstname, lastname, email, tel, map_marker_id)
+      VALUES
+        (?, ?, ?, ?, ?);
+      `, [ firstname, lastname, email, tel, map_marker_id ])
 
-	return result
+    return result
+     
+  } catch(error) {
+    console.error(error)
+    return error
+  }
 }
 
 module.exports = {
